72bea1c2d9ee0e605d63abcb9611c069120a9940
OmarAbouzeid95 <98395747+OmarAbouzeid95@users.noreply.github.com>
2022-06-07 21:22:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const okBtn = document.getElementById("ok");
 const tdCell = document.getElementById("cell");
 
-okBtn.addEventListener("touchstart", () => {
+okBtn.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
     tdCell.innerText = "ok";
 });
 
